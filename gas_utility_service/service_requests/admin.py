cb25546874eb27c5330f73b66d8b581f76226628
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-14 23:09:48 +0530

    Update admin.py

--- gas_utility_service/service_requests/admin.py
+++ gas_utility_service/service_requests/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import ServiceRequest
 
+# Admin interface customization
 @admin.register(ServiceRequest)
 class ServiceReuqestAdmin(admin.ModelAdmin):
     list_display = ('customer','service_type','status','created_at')
